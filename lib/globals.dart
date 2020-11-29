6cca2d9a48492c3e0375faff33a9b9976dac2ec9
idbella <idbellasaid@gmail.com>
2020-11-29 23:26:53 +0100

    listing doctors

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,5 @@
 
+import 'package:PatientMonitorMobileApp/models/Doctor.dart';
 import 'package:PatientMonitorMobileApp/models/insurance.dart';
 import 'package:flutter/material.dart';
 import 'package:PatientMonitorMobileApp/models/user.dart';
@@ -13,10 +14,11 @@ class AccountType{
 
 class Globals {
 
-	static String				url					= 'https://idbella.herokuapp.com';
+	static String				url					= 'http://172.16.176.200:8080';
 	static List<User>			usersList			= List();
 	static List<Patient>		patientsList;
 	static List<Insurance>	insuarnces			= List();
+	static List<Doctor>		doctors				= List();
 	static int					adminId				= 1;
 	static int					nurseId				= 3;
 	static int					doctorId				= 2;
@@ -51,5 +53,31 @@ class Globals {
 			else
 				print('error ' + response.content().toString());
 		});
+	}
+
+	static void getDoctors({Function callback})
+	{
+		if (Globals.insuarnces.isNotEmpty)
+			return ;
+		Requests.get(Globals.url + '/api/doctors').then((Response response){
+			print('doctors : ' + response.content().toString());
+			if (response.statusCode == 200)
+			{
+				List<dynamic> list = response.json();
+				list.forEach((json) {
+					doctors.add(Doctor.fromJson(json));
+				});
+				if (callback != null)
+					callback();
+			}
+			else
+				print('error ' + response.content().toString());
+		});
+	}
+
+	static void init()
+	{
+		getInsurances();
+		getDoctors();
 	}
 }
